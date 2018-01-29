49fc1679da9095bed6ac40f9c11188dfc85435ce
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2018-01-29 18:47:24 +0530

    Submission ready. Make pipe mode commands dynamic

--- Assignment2/Ass2_48.cpp
+++ Assignment2/Ass2_48.cpp
@@ -1,10 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <bits/stdc++.h>
+#include <sys/stat.h> 
+#include <sys/types.h> 
 #include <fcntl.h>
 #include <cstdlib>
-#include <bits/stdc++.h>
-#include <string>
-#include <iostream>
+#include <string> 
 using namespace std;
 
 int main(){
@@ -14,40 +16,19 @@ int main(){
   while(flag){
     //UI
     cout << "Press key corresponding to the task needed." <<endl;
-    cout << "1. Run an external command"<<endl;
-    cout << "2. Run an internal command"<<endl;
+    cout << "1. Run an internal command"<<endl;
+    cout << "2. Run an external command"<<endl;
     cout << "3. Run an external command by redirecting standard input from a file"<<endl;
     cout << "4. Run an external command by redirecting standard output to a file"<<endl;
     cout << "5. Run an external command in the background"<<endl;
     cout << "6. Run several external commands in pipe mode"<<endl;
     cout << "7. Quit the shell." << endl;
     cin >> choice;
+    getline(cin, token);
 
     switch(choice){
     case 1:
-      if(strcmp(token.c_str(), "chdir") == 0){
-	if (args.size() > 1)
-	  chdir(args[0].c_str());
-	else
-	  cout<< "Please specify the directory to change to" <<endl;
-	return 0;
-      } else if(strcpm(toke.c_str(), "mkdir") == 0){
-	if (args.size() > 1)
-	  mkdir(args[0].c_str());
-	else
-	  cout<< "Please specify the directory to create" <<endl;
-	return 0;
-      }  else if(strcpm(toke.c_str(), "rmdir") == 0){
-	if (args.size() > 1)
-	  rmdir(args[0].c_str());
-	else
-	  cout<< "Please specify the directory to delete" <<endl;
-	return 0;
-      }
-      break;
-    case 2:
       {
-	// Input
 	cin >> token;
 	vector<string> args;
 	string temp;
@@ -55,7 +36,37 @@ int main(){
 	  cin >> temp;
 	  args.push_back(temp);
 	}
-
+	
+	if(strcmp(token.c_str(), "chdir") == 0){
+	  if (args.size() >= 1)
+	    chdir(args[0].c_str());
+	  else
+	    cout<< "Please specify the directory to change to" <<endl;
+	  return 0;
+	} else if(strcmp(token.c_str(), "mkdir") == 0){
+	  if (args.size() >= 1)
+	    mkdir(args[0].c_str(), S_IRWXU|S_IRWXG|S_IROTH|S_IXOTH);
+	  else
+	    cout<< "Please specify the directory to create" <<endl;
+	}  else if(strcmp(token.c_str(), "rmdir") == 0){
+	  if (args.size() >= 1)
+	    rmdir(args[0].c_str());
+	  else
+	    cout<< "Please specify the directory to delete" <<endl;
+	}
+	break;
+      }
+    case 2:
+      {
+	// Input
+	
+	vector<string> args;
+	string temp;
+	while(cin.peek() != '\n'){
+	  cin >> temp;
+	  args.push_back(temp);
+	}
+	
 	// Forking
 	if ((pid = fork()) < 0)
 	  perror("fork error");
@@ -64,36 +75,38 @@ int main(){
 	  for(int i = 0; i < args.size(); i++)
 	    arglist[i] = strdup(args[i].c_str());
 	  arglist[args.size()] = NULL;
-	  if(args.size() > 0){
-	    execvp(token.c_str(), arglist);
-	  }
-	  else{
-	    execlp(token.c_str(), token.c_str(), NULL);
-	  }
+	  if(args.size() > 0)
+	    execvp(*arglist, arglist);
 	  flag = false;
 	  break;
 	} else {
-	  wait(&res);
+	  waitpid(pid, NULL, 0);
+	  continue;
 	}
 	break;
       }
     case 3:
       {
-	close(0);
-	int fs = open ("myfile.txt", O_WRONLY | O_CREAT);
-	if(fs < 0){
-	  cout<<"Can¡¯t read file using file\n"<<endl;
-	  exit (1);
-	}
-	dup(fs);
-
 	// Input
-	cin >> token;
 	vector<string> args;
 	string temp;
+	bool direction_op = false; 
+	string file_name;
 	while(cin.peek() != '\n'){
 	  cin >> temp;
-	  args.push_back(temp);
+	  if(strcmp(temp.c_str(), "<") == 0){
+	    direction_op = true;
+	  } else if(direction_op == false){
+	    args.push_back(temp);
+	  } else{
+	    file_name = temp;
+	    break;
+	  }
+	}
+	int fs = open(file_name.c_str(), O_RDONLY);
+	if(fs < 0){
+	  cout<<"Can't read file using file\n"<<endl;
+	  break;
 	}
 
 	// Forking
@@ -104,38 +117,39 @@ int main(){
 	  for(int i = 0; i < args.size(); i++)
 	    arglist[i] = strdup(args[i].c_str());
 	  arglist[args.size()] = NULL;
-	  if(args.size() > 0){
-	    execvp(token.c_str(), arglist);
-	  }
-	  else{
-	    execlp(token.c_str(), token.c_str(), NULL);
-	  }
+	  close(0);
+	  dup(fs);
+	  if(args.size() > 0)
+	    execvp(*arglist, arglist);
 	  flag = false;
 	  break;
 	} else {
-	  wait(&res);
+	  waitpid(pid, NULL, 0);
+	  close(fs);
 	}
-	close(fs);
-	dup(0);
 	break;
       }
     case 4:
       {
-	close(1);
-	int fs = open ("myfile.txt", O_WRONLY | O_CREAT);
-	if(fs < 0){
-	  cout << "Can¡¯t read file using file\n"<< endl;
-	  exit (1);
-	}
-	dup(fs);
-
 	// Input
-	cin >> token;
 	vector<string> args;
 	string temp;
-	while(cin.peek() != '\n'){
-	  cin >> temp;
-	  args.push_back(temp);
+	bool direction_op = false; 
+	string file_name;
+	while(cin >> temp){
+	  if(strcmp(temp.c_str(), ">") == 0){
+	    direction_op = true;
+	  } else if(direction_op == true){
+	    file_name = temp;
+	    break;
+	  } else{
+	    args.push_back(temp);
+	  }
+	}
+	int fs = open(file_name.c_str(), O_WRONLY | O_CREAT | O_SYNC, 0666);
+	if(fs < 0){
+	  cout<<"Can't read file using file\n"<<endl;
+	  break;
 	}
 
 	// Forking
@@ -146,32 +160,28 @@ int main(){
 	  for(int i = 0; i < args.size(); i++)
 	    arglist[i] = strdup(args[i].c_str());
 	  arglist[args.size()] = NULL;
-	  if(args.size() > 0){
-	    execvp(token.c_str(), arglist);
-	  }
-	  else{
-	    execlp(token.c_str(), token.c_str(), NULL);
-	  }
+	  close(1);
+	  dup(fs);
+	  if(args.size() > 0)
+	    execvp(*arglist, arglist);
 	  flag = false;
 	  break;
 	} else {
-	  wait(&res);
+	  waitpid(pid, NULL, 0);
+	  close(fs);
 	}
-	close(fs);
-	dup(1);
 	break;
       }
     case 5:
       {
 	// Input
-	cin >> token;
 	vector<string> args;
 	string temp;
 	while(cin.peek() != '\n'){
 	  cin >> temp;
 	  args.push_back(temp);
 	}
-
+	
 	// Forking
 	if ((pid = fork()) < 0)
 	  perror("fork error");
@@ -180,81 +190,129 @@ int main(){
 	  for(int i = 0; i < args.size(); i++)
 	    arglist[i] = strdup(args[i].c_str());
 	  arglist[args.size()] = NULL;
-	  if(args.size() > 0){
-	    execvp(token.c_str(), arglist);
-	  }
-	  else{
-	    execlp(token.c_str(), token.c_str(), NULL);
-	  }
+	  if(args.size() > 0)
+	    execvp(*arglist, arglist);
 	  flag = false;
 	  break;
 	}
 	break;
       }
-    case 6:
-      {
-	bool sub_cmd = true;
-	int count = 0;
-	int mypipe1[2], mypipe2[2];
-	while(sub_cmd){
-	  // Input
-	  cin >> token;
-	  vector<string> args;
-	  string temp;
-	  
-	  while(cin.peek() != '\n'){
-	    cin >> temp;
-	    args.push_back(temp);
-	    if(strcmp(args[i].c_str(),"|") == 0){
-	      if(count == 0){
-		if (pipe(mypipe1) || pipe(mypipe2)){
-		  fprintf(stderr, "Pipe failed.\n");
-		  return EXIT_FAILURE;
-		}
-		close(1);
-		dup(mypipe1[1]);
-		close(mypipe1[0]);
+    case 6: {
+      string s;
+      char dump;
+      getline(cin,s);
+      stringstream str(s);
+      string token;
+      int n=0;
+      while(str >> token){
+	n++;
+      }
 
-		// Forking
-		if ((pid = fork()) < 0)
-		  perror("fork error");
-		else if (pid == 0) {
-		  char* arglist[args.size()+1];
-		  for(int i = 0; i < args.size(); i++)
-		    arglist[i] = strdup(args[i].c_str());
-		  arglist[args.size()] = NULL;
-		  if(args.size() > 0){
-		    execvp(token.c_str(), arglist);
-		  }
-		  else{
-		    execlp(token.c_str(), token.c_str(), NULL);
-		  }
-		  flag = false;
-		  break;
-		} else {
-		  wait(&res);
-		}
-		close(mypipe[1]);
-		dup(mypipe[0]);
-		close(0);
-		dup(mypipe[1]);
-	      } else if(i%2==1){
-		close(mypipe[1]);
-		dup(mypipe[0]);
-		close(mypipe[0]);
-		dup(mypipe[1]);
-	      }
-	      count++;
-	      break;
-	    }
+      char** arglist = new char*[n];
+      stringstream str2(s);
+      int i = 0;
+      while(str2 >> token){
+	char *arg = new char[token.size()+1];
+	copy(token.begin(),token.end(),arg);
+	arg[token.size()]='\0';
+	arglist[i] = arg;
+	i++;
+      }
+      
+      int length = i;
+      int commandnum = 0;
+      for (i=0;i<length;i++){
+	if (strcmp(arglist[i],"|") == 0)
+	  {
+	    commandnum++;
 	  }
-	  if(cin.peek() == '\n'){
-	    sub_cmd = false;
-	    break;
-	  }
+      }
+      
+      char** newarglist[commandnum+1];
+      int k, j=0;
+      i=0;
+      while(i < length){
+	k = i;
+	int l = 0;
+	while(strcmp(arglist[k],"|")!=0 && k < length){
+	  k++;
+	  l++;
+	  if(k >= length) break;
 	}
+	newarglist[j] = new char*[l+1];
+	
+	k = i; l = 0;
+	while(strcmp(arglist[k],"|")!=0 && k < length){
+	  newarglist[j][l] = arglist[k];
+	  k++;
+	  l++;
+	  if(k >= length) break;
+	}
+	newarglist[j][l] = 0;
+	
+	j++;
+	k++;
+	i = k;
+      }
+      
+      int pipef[2];
+      if( pipe(pipef) < 0 ) {
+	perror("pipe");
+	fflush(stderr);
+      }
+      /*
+      for(int i = 0; i< commandnum; i++){
+	int pid = fork();
+	if(pid == 0 && i == 0){
+	  close(STDOUT_FILENO);
+	  dup(pipef[1]);
+	  close(pipef[0]);
+	  execvp(newarglist[i][0],newarglist[i]);
+	}
+	else if(pid == 0){
+	    close(STDIN_FILENO);
+	    dup(pipef[0]);
+	    close(pipef[1]);
+	    close(STDOUT_FILENO);
+	    dup(pipef[1]);
+	    close(pipef[0]);
+	    execvp(newarglist[i][0],newarglist[i]);
+	} else {
+	  waitpid(pid, NULL, 0);
+	}
+      }
+      int pid = fork();
+      if(pid == 0){
+	close(STDIN_FILENO);
+	dup(pipef[0]);
+	close(pipef[1]);
+	execvp(newarglist[commandnum][0],newarglist[commandnum]);
+      } else {
+	waitpid(pid, NULL, 0);
+	} */
+      
+      pid_t newp = fork();
+      if(newp == 0){
+	close(STDOUT_FILENO);
+	dup(pipef[1]);
+	close(pipef[0]);
+	execvp(newarglist[0][0],newarglist[0]);
+	flag = false;
 	break;
       }
+      else{
+	pid_t newp2 = fork();
+	if(newp2 == 0){
+	  close(STDIN_FILENO);
+	  dup(pipef[0]);
+	  close(pipef[1]);
+	  execvp(newarglist[1][0],newarglist[1]);
+	  flag = false;
+	  break;
+	}
+      }
+      break;
+    }
     case 7:{
       flag = false;
       break;
